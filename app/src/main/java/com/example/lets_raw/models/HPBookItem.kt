6b3b7a7fc299b2f2fc4ffadd634ed54257f7089f
sly987 <diaomouhamedmountaga@gmail.com>
2025-03-01 00:55:18 +0100

    Added continue reading recycler view and fixed some issues with UI

--- app/src/main/java/com/example/lets_raw/models/HPBookItem.kt
+++ app/src/main/java/com/example/lets_raw/models/HPBookItem.kt
@@ -1,3 +1,3 @@
 package com.example.lets_raw.models
 
-data class HPBookItem(val ID: Int, val title: String, val coverURL: String)
+data class HPBookItem(val ID: Int, val title: String, val author: String , val coverURL: String, val pages: Int, val pagesRead: Int)
